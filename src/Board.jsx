e392b2f02ab893e209093f1bf8982131f0c88cac
SmitasCodes <airidassmith30@gmail.com>
2026-01-21 19:04:12 +0200

    first commit

---
+++ src/Board.jsx
@@ -0,0 +1,106 @@
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+const WINNING_COMBINATIONS = [
+  [
+    { row: 0, col: 0 },
+    { row: 0, col: 1 },
+    { row: 0, col: 2 },
+  ],
+  [
+    { row: 1, col: 0 },
+    { row: 1, col: 1 },
+    { row: 1, col: 2 },
+  ],
+  [
+    { row: 2, col: 0 },
+    { row: 2, col: 1 },
+    { row: 2, col: 2 },
+  ],
+  [
+    { row: 0, col: 0 },
+    { row: 1, col: 0 },
+    { row: 2, col: 0 },
+  ],
+  [
+    { row: 0, col: 1 },
+    { row: 1, col: 1 },
+    { row: 2, col: 1 },
+  ],
+  [
+    { row: 0, col: 2 },
+    { row: 1, col: 2 },
+    { row: 2, col: 2 },
+  ],
+  [
+    { row: 0, col: 0 },
+    { row: 1, col: 1 },
+    { row: 2, col: 2 },
+  ],
+  [
+    { row: 0, col: 2 },
+    { row: 1, col: 1 },
+    { row: 2, col: 0 },
+  ],
+];
+
+import React from "react";
+
+const Board = () => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const activePlayer = () => {
+    let xCols = 0;
+    let oCols = 0;
+
+    for (let row of gameBoard) {
+      for (let col of row) {
+        if (col === "X") xCols++;
+        if (col === "0") oCols++;
+      }
+    }
+
+    return xCols === oCols ? "X" : "0";
+  };
+
+  const buttonHandler = (rowIndex, colIndex) => {
+    setGameBoard((prevGameBoard) => {
+      let updatedGameBoard = [...prevGameBoard];
+      const updatedRow = [...prevGameBoard[rowIndex]];
+      updatedRow[colIndex] = activePlayer();
+      updatedGameBoard[rowIndex] = updatedRow;
+      return updatedGameBoard;
+    });
+  };
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const first = gameBoard[combination[0].row][combination[0].col];
+    const second = gameBoard[combination[1].row][combination[1].col];
+    const third = gameBoard[combination[2].row][combination[2].col];
+
+    if (first && first === second && second === third) {
+      console.log("chicken dinner");
+    }
+  }
+
+  return (
+    <div className="h-96 w-96 bg-red-200 grid grid-flow-row grid-cols-3 gap-2">
+      {gameBoard.map((row, rowIndex) =>
+        row.map((col, colIndex) => (
+          <button
+            className="bg-red-400 cursor-pointer"
+            key={colIndex}
+            onClick={() => buttonHandler(rowIndex, colIndex)}
+          >
+            {col}
+          </button>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Board;
