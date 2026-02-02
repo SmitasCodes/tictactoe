fa8372d728bc54b058c70b2d7e3fd6510d1caf7b
SmitasCodes <airidassmith30@gmail.com>
2026-02-02 14:28:18 +0200

    added draw, fixed grid positioning

--- src/Board.jsx
+++ src/Board.jsx
@@ -8,7 +8,10 @@ const Board = ({ players, setGameStart }) => {
     player2: { name: player2Name, symbol: player2Symbol },
   } = players;
 
+  const moves = gameBoard.flat().filter(Boolean).length;
+
   const winner = useMemo(() => {
+    if (moves === 9) return "draw";
     for (const combination of WINNING_COMBINATIONS) {
       const a = gameBoard[combination[0].row][combination[0].col];
       const b = gameBoard[combination[1].row][combination[1].col];
@@ -22,7 +25,6 @@ const Board = ({ players, setGameStart }) => {
     return null;
   }, [gameBoard]);
 
-  const moves = gameBoard.flat().filter(Boolean).length;
   const activePlayer = winner
     ? null
     : moves % 2 === 0
@@ -32,6 +34,7 @@ const Board = ({ players, setGameStart }) => {
   const buttonHandler = (rowIndex, colIndex) => {
     if (winner) return;
     if (gameBoard[rowIndex][colIndex]) return;
+
     setGameBoard((prevGameBoard) => {
       let updatedGameBoard = [...prevGameBoard];
       const updatedRow = [...prevGameBoard[rowIndex]];
@@ -57,7 +60,7 @@ const Board = ({ players, setGameStart }) => {
           <span className="bg-blue-900 px-1.5 rounded-lg">{player2Symbol}</span>
         </li>
       </ul>
-      <div className="h-96 w-96 bg-red-200 grid grid-flow-row grid-cols-3 gap-2">
+      <div className="h-96 w-96 bg-red-200 grid grid-rows-3 grid-cols-3 gap-2">
         {gameBoard.map((row, rowIndex) =>
           row.map((col, colIndex) => (
             <button
@@ -74,7 +77,9 @@ const Board = ({ players, setGameStart }) => {
       {winner && (
         <div className="absolute bg-gray-900/90 inset-0 flex justify-center items-center">
           <div className="bg-yellow-400 text-center flex-wrap w-102 p-6 rounded-md">
-            <h2 className="font-bold pb-3 text-3xl">Winner is {winner}</h2>
+            <h2 className="font-bold pb-3 text-3xl">
+              {winner === "draw" ? "Draw" : "Winner is " + winner}
+            </h2>
             <div className="flex justify-around w-full">
               <button
                 className="bg-amber-600 px-2 rounded-xl text-lg cursor-pointer"
